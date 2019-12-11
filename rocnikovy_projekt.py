11434a46329b57dda3ef5cb8df53e6424fc92955
Richard Biro <biro.risko@gmail.com>
2019-12-11 18:53:24 +0100

    Pridana moznost nahodneho generovania

--- rocnikovy_projekt.py
+++ rocnikovy_projekt.py
@@ -1,44 +1,41 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 import random
+import cProfile
 
 
-already_known = 1
-want_to_know = 20
-solutions_xxexx = dict()
-all_solutions_xxexx = []
 
+solutions = dict()
+correct_solutions = []
 relatively_prime = []
 labels = []
 six_squares = []
 seven_squares = []
 
-
-
-
-
-def gcd(a,b):
-    return math.gcd(a,b)
-
 def gcd_array(X):
-    d = gcd(X[0],X[1])
-    for i in range(2,len(X)): d = gcd(d,X[i])
+    if len(X) < 2: return None
+    d = math.gcd(X[0],X[1])
+    for i in range(2,len(X)): d = math.gcd(d,X[i])
     return d
 
-def lcm(a,b): return a*b//gcd(a,b)
-
-def lcm_array(X):
-    m = lcm(X[0],X[1])
-    for i in range(2,len(X)): m = lcm(m,X[i])
-    return m
 
 def is_square(n):
     if n < 0: return False
     return (int(math.sqrt(n))**2 == n)
 
 
-def add_solution_xxexx(X,u1,v1,u2,v2):
+def get_solutions():
+    return solutions
+
+
+def add_correct_solution(X,u1,v1,u2,v2):
+    if [X,u1,v1,u2,v2] not in correct_solutions:
+        correct_solutions.append([X,u1,v1,u2,v2])
+        
+
+def add_solution(X,u1,v1,u2,v2):
     different = len(set(X))
 
     if different == 9:
@@ -56,18 +53,17 @@ def add_solution_xxexx(X,u1,v1,u2,v2):
             index = ' '.join(str(s) for s in [corner1,corner2,center])
             largest = max(u1,v1,u2,v2)
 
-            if index not in solutions_xxexx or list(map(int,solutions_xxexx[index].split(' ')))[0] > largest:
-                solutions_xxexx[index] = ' '.join(str(s) for s in [max(u1,v1,u2,v2),squares])
+            if index not in solutions or list(map(int,solutions[index].split(' ')))[0] > largest:
+                solutions[index] = ' '.join(str(s) for s in [max(u1,v1,u2,v2),squares])
 
                 if squares == 6: six_squares[largest] += 1
-                else: seven_squares[largest] += 1
-                             
-                    
+                else:
+                    seven_squares[largest] += 1
+                    add_correct_solution(X,u1,v1,u2,v2)
 
 
 
-
-def run_configuration_xxexx(u1,v1,u2,v2):
+def run_configuration(u1,v1,u2,v2):
     E = abs((u1**2 + v1**2)*(u2**2 + v2**2))
     A = abs((u1**2 + v1**2)*(u2**2 + 2*u2*v2 - v2**2))
     B = abs((u1**2 + 2*u1*v1 - v1**2)*(u2**2 + v2**2))
@@ -84,7 +80,7 @@ def run_configuration_xxexx(u1,v1,u2,v2):
     G = 3*E - H - I
     D = 3*E - A - G
     F = 3*E - C - I
-    add_solution_xxexx([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
+    add_solution([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
 
 
 
@@ -104,7 +100,7 @@ def run_configuration_xxexx(u1,v1,u2,v2):
     G = 3*E - H - I
     D = 3*E - A - G
     F = 3*E - C - I
-    add_solution_xxexx([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
+    add_solution([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
 
 
 
@@ -124,7 +120,7 @@ def run_configuration_xxexx(u1,v1,u2,v2):
     D = 3*E - A - G
     F = 3*E - C - I
     H = 3*E - G - I
-    add_solution_xxexx([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
+    add_solution([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
 
 
 
@@ -145,39 +141,57 @@ def run_configuration_xxexx(u1,v1,u2,v2):
     C = (D + H)//2
     G = (B + F)//2
     I = (B + D)//2
-    add_solution_xxexx([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
+    add_solution([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
 
 
-
-
-def add_all_pairs():
-    for i in range(already_known, want_to_know+1):
-        labels.append(i-1)
+def add_all_labels(upper_bound):
+    for i in range(upper_bound+1):
+        labels.append(i)
         six_squares.append(0)
         seven_squares.append(0)
-        if i < want_to_know:
-            for j in range(already_known+1,want_to_know):
-                if gcd(i,j) == 1:
-                    relatively_prime.append([i,j])
+
+
+def get_pairs():
+    return relatively_prime
+
+
+def add_all_pairs(upper_bound):
+    add_all_labels(upper_bound)
+    
+    for i in range(1,upper_bound+1):
+        for j in range(1,upper_bound+1):
+            if math.gcd(i,j) == 1:
+                relatively_prime.append([i,j])
 
     relatively_prime.sort()
 
 
 
-def process():
-    progress = 10
-    n = len(relatively_prime)
 
-    print("PROCESSING",n)
+
+def run_all_pairs():
+    progress = 0
+    
+    n = len(relatively_prime)
+    allsteps = n*(n-1)//2
+    step = 0
+    
+    print("START |",str(datetime.now()))
 
     for i1 in range(n):
-        print(i1,"out of",n)
         u1 = relatively_prime[i1][0]
         v1 = relatively_prime[i1][1]
         for i2 in range(i1+1,n):
             u2 = relatively_prime[i2][0]
             v2 = relatively_prime[i2][1]
-            run_configuration_xxexx(u1,v1,u2,v2)
+            run_configuration(u1,v1,u2,v2)
+            step += 1
+            if step/allsteps * 100 > progress:
+                print(progress,"% |",step,"out of",allsteps)
+                progress += 5
+                
+    print("END |",str(datetime.now()),"\n")
+                
 
 
 
@@ -195,15 +209,80 @@ def show_graph():
     ax.legend()
 
     fig.tight_layout()
-
     plt.show()
 
+
+
+def random_relatively_prime(upper_bound):
+    a = random.randint(1,upper_bound-1)
+    b = random.randint(1,upper_bound-1)
+    d = math.gcd(a,b)
+    return [a//d,b//d]
+
+
+
+def random_search(upper_bound,k):
+    add_all_labels(upper_bound)
+    for _ in range(k):
+        first_pair = random_relatively_prime(upper_bound)
+        second_pair = random_relatively_prime(upper_bound)
+        run_configuration(first_pair[0],first_pair[1],second_pair[0],second_pair[1])
     
 
+
 def main():
-    add_all_pairs()
-    process()
-    show_graph()
+    
+    while True:
+        print("Type 'fixed' to search all solutions.")
+        print("Type 'random' to search random solutions.")
+        answer = input()
+        if answer == "fixed":
+            search_fixed = True
+            break
+        
+        elif answer == "random":
+            search_fixed = False
+            break
+        
+        else: print("Invalid command:",answer,"\n")
+            
+    
+    while True:
+        try:
+            print("\nChoose a positive integer - upper bound for solutions:")
+            print("Tip: fixed search for upper bound above 70 may run for a very long time.")
+            print("Tip: fixed search for upper bound above 200 is currently out of reach.")
+                
+            answer = input()
+            upper_bound = int(answer)
+            break
+        
+        except ValueError: print("Invalid positive integer",answer)
+        
+
+    if search_fixed:
+        add_all_pairs(upper_bound)
+        cProfile.run('run_all_pairs()')
+        show_graph()
+
+    else:
+        while True:
+            try:
+                print("\nChoose a positive integer - number of iterations:")      
+                answer = input()
+                k = int(answer)
+                break
+        
+            except ValueError: print("Invalid positive integer",answer)
+     
+        random_search(upper_bound,k)
+
+    print("")
+    print(len(solutions) - len(correct_solutions),"solutions with six squares were found.")
+    print(len(correct_solutions),"solutions with at least seven squares were found.")
+    for i in correct_solutions: print(i)
+
+
 
 
 if __name__ == '__main__':
--- rocnikovy_projekt_test.py
+++ rocnikovy_projekt_test.py
@@ -1,27 +1,60 @@
 import rocnikovy_projekt
 import pytest
-import subprocess
-import sys
-import os
 import random
+import math
+
+iterations = 5000
+square_range = 10**12
+relatively_prime_range = 100
 
 
-def test_random_positive_square():
-    for _ in range(5000):
-        n = random.randint(1,10**12)
+def test_square_random_positive_square():
+    for _ in range(iterations):
+        n = random.randint(1,square_range)
         assert rocnikovy_projekt.is_square(n**2) is True
 
-def test_random_positive_not_square():
-    for _ in range(5000):
-        m = random.randint(2,10**12)
+
+def test_square_random_positive_not_square():
+    for _ in range(iterations):
+        m = random.randint(2,square_range)
         n = random.randint(m**2 + 1, (m + 1)**2 - 1)
         assert rocnikovy_projekt.is_square(n) is False
 
-def test_random_negative():
-    for _ in range(5000):
-        n = - random.randint(1,10**12)
+
+def test_square_random_negative():
+    for _ in range(iterations):
+        n = - random.randint(1,square_range)
         assert rocnikovy_projekt.is_square(n) is False
 
-def test_zero():
+
+def test_square_zero():
     assert rocnikovy_projekt.is_square(0) is True
+
+
+def test_relatively_prime_one_one_relatively_prime():
+    rocnikovy_projekt.add_all_pairs(1)
+    assert [1,1] in rocnikovy_projekt.get_pairs()
+
+
+def test_relatively_prime_random_relatively_prime():
+    rocnikovy_projekt.add_all_pairs(relatively_prime_range+1)
+    for _ in range(iterations):
+        a = random.randint(1,relatively_prime_range)
+        b = random.randint(1,relatively_prime_range)
+        d = math.gcd(a,b)
+        a = a//d
+        b = b//d
+        assert [a,b] in rocnikovy_projekt.get_pairs()
+
+
+def test_relatively_prime_random_not_relatively_prime():
+    rocnikovy_projekt.add_all_pairs(relatively_prime_range+1)
+    for _ in range(iterations):
+        d = random.randint(2,int(math.sqrt(relatively_prime_range)))
+        a = d*random.randint(1,relatively_prime_range//d)
+        b = d*random.randint(1,relatively_prime_range//d)
+        assert [a,b] not in rocnikovy_projekt.get_pairs()
+
+
+
 
