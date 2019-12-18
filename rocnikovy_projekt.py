ca1a1ec6f4047bf68f3f9b6ad7a6b39bbe6381ad
Richard Biro <biro.risko@gmail.com>
2019-12-18 23:09:46 +0100

    uprava kodu, pridane testy pre stvorce

--- rocnikovy_projekt.py
+++ rocnikovy_projekt.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import random
 import cProfile
-
-
+from mpl_toolkits.mplot3d import Axes3D
 
 solutions = dict()
 correct_solutions = []
@@ -14,10 +13,10 @@ labels = []
 six_squares = []
 seven_squares = []
 
-def gcd_array(X):
-    if len(X) < 2: return None
-    d = math.gcd(X[0],X[1])
-    for i in range(2,len(X)): d = math.gcd(d,X[i])
+def gcd_array(array):
+    if len(array) < 2: return None
+    d = math.gcd(array[0],array[1])
+    for i in range(2,len(array)): d = math.gcd(d,array[i])
     return d
 
 
@@ -30,118 +29,114 @@ def get_solutions():
     return solutions
 
 
-def add_correct_solution(X,u1,v1,u2,v2):
-    if [X,u1,v1,u2,v2] not in correct_solutions:
-        correct_solutions.append([X,u1,v1,u2,v2])
+def add_correct_solution(array,u1,v1,u2,v2):
+    if [array,u1,v1,u2,v2] not in correct_solutions:
+        correct_solutions.append([array,u1,v1,u2,v2])
         
 
-def add_solution(X,u1,v1,u2,v2):
-    different = len(set(X))
+def add_solution(array,u1,v1,u2,v2):
+    different = len(set(array))
 
     if different == 9:
         squares = 0
-        for i in X:
+        for i in array:
             if is_square(i): squares += 1
 
         if squares > 5:
-            corners = [X[0],X[2],X[6],X[8]]
+            corners = [array[0],array[2],array[6],array[8]]
             corners.sort()
             corner1 = corners[0]
             corner2 = corners[1]
-            center = X[4]
+            center = array[4]
             
             index = ' '.join(str(s) for s in [corner1,corner2,center])
             largest = max(u1,v1,u2,v2)
 
             if index not in solutions or list(map(int,solutions[index].split(' ')))[0] > largest:
-                solutions[index] = ' '.join(str(s) for s in [max(u1,v1,u2,v2),squares])
+                solutions[index] = ' '.join(str(s) for s in [largest,squares])
+                center = int(math.sqrt(center))
 
                 if squares == 6: six_squares[largest] += 1
+                        
                 else:
                     seven_squares[largest] += 1
-                    add_correct_solution(X,u1,v1,u2,v2)
+                    add_correct_solution(array,u1,v1,u2,v2)
+
+
+def evaluate_rows(array):
+    for i in range(3):
+        for j in range(3):
+            if array[3*i+j%3] != None and array[3*i+(j+1)%3] != None and array[3*i+(j+2)%3] == None:
+                    array[3*i+(j+2)%3] = 3*array[4] - array[3*i+j%3] - array[3*i+(j+1)%3]
+
+
+def evaluate_columns(array):
+    for i in range(3):
+        for j in range(3):
+            if array[i+3*(j%3)] != None and array[i+3*((j+1)%3)] != None and array[i+3*((j+2)%3)] == None:
+                array[i+3*((j+2)%3)] = 3*array[4] - array[i+3*(j%3)] - array[i+3*((j+1)%3)]
+
+
+
+def evaluate_two_edges(array):
+    for i in range(2):
+        for j in range(2):
+            if array[1+6*i] != None and array[3+2*j] != None:
+                array[8-6*i-2*j] = (array[1+6*i] + array[3+2*j])//2
+
+
+def evaluate_square(array):
+    evaluate_rows(array)
+    evaluate_columns(array)
+    evaluate_two_edges(array)
+
+    if None in array: return None
+    return array
+
+
+
+def normalize(array,divisor):
+    for i in range(len(array)): array[i] = (array[i]//divisor)**2
 
 
 
 def run_configuration(u1,v1,u2,v2):
-    E = abs((u1**2 + v1**2)*(u2**2 + v2**2))
-    A = abs((u1**2 + v1**2)*(u2**2 + 2*u2*v2 - v2**2))
-    B = abs((u1**2 + 2*u1*v1 - v1**2)*(u2**2 + v2**2))
-    H = abs((- u1**2 + 2*u1*v1 + v1**2)*(u2**2 + v2**2))
-    I = abs((u1**2 + v1**2)*(- u2**2 + 2*u2*v2 + v2**2))
+    constants = [abs((u1**2 + v1**2)*(u2**2 + v2**2)),
+                 abs((u1**2 + v1**2)*(u2**2 + 2*u2*v2 - v2**2)),
+                 abs((u1**2 + v1**2)*(- u2**2 + 2*u2*v2 + v2**2)),
+                 abs((u1**2 + 2*u1*v1 - v1**2)*(u2**2 + v2**2)),
+                 abs((- u1**2 + 2*u1*v1 + v1**2)*(u2**2 + v2**2))]
+    
+    divisor = gcd_array(constants)
+    normalize(constants,divisor)
 
-    divisor = gcd_array([A,B,E,H,I])
-    E = (E//divisor)**2
-    A = (A//divisor)**2
-    B = (B//divisor)**2
-    H = (H//divisor)**2
-    I = (I//divisor)**2
-    C = 3*E - A - B
-    G = 3*E - H - I
-    D = 3*E - A - G
-    F = 3*E - C - I
-    add_solution([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
+    center = constants[0]
+    x1 = constants[1]
+    x2 = constants[2]
+    y1 = constants[3]
+    y2 = constants[4]
+    squares = []
 
+    squares.append(evaluate_square([x1,   y1,     None,
+                                    None, center, None,
+                                    None, y2,     x2]))
 
+    squares.append(evaluate_square([x1,   y2,     None,
+                                    None, center, None,
+                                    None, y1,     x2]))
 
-    E = abs((u1**2 + v1**2)*(u2**2 + v2**2))
-    A = abs((u1**2 + v1**2)*(u2**2 + 2*u2*v2 - v2**2))
-    H = abs((u1**2 + 2*u1*v1 - v1**2)*(u2**2 + v2**2))
-    B = abs((- u1**2 + 2*u1*v1 + v1**2)*(u2**2 + v2**2))
-    I = abs((u1**2 + v1**2)*(- u2**2 + 2*u2*v2 + v2**2))
+    squares.append(evaluate_square([x1,   None,   y1,
+                                    None, center, None,
+                                    y2,   None,   x2]))
 
-    divisor = gcd_array([A,B,E,H,I])
-    E = (E//divisor)**2
-    A = (A//divisor)**2
-    B = (B//divisor)**2
-    H = (H//divisor)**2
-    I = (I//divisor)**2
-    C = 3*E - A - B
-    G = 3*E - H - I
-    D = 3*E - A - G
-    F = 3*E - C - I
-    add_solution([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
+    squares.append(evaluate_square([None, x1,     None,
+                                    y1,   center, y2,
+                                    None, x2,     None]))
+    
+    for i in squares:
+        if i != None: add_solution(i,u1,v1,u2,v2)
 
 
-
-    E = abs((u1**2 + v1**2)*(u2**2 + v2**2))
-    A = abs((u1**2 + v1**2)*(u2**2 + 2*u2*v2 - v2**2))
-    C = abs((u1**2 + 2*u1*v1 - v1**2)*(u2**2 + v2**2))
-    G = abs((- u1**2 + 2*u1*v1 + v1**2)*(u2**2 + v2**2))
-    I = abs((u1**2 + v1**2)*(- u2**2 + 2*u2*v2 + v2**2))
-
-    divisor = gcd_array([A,C,E,G,I])
-    E = (E//divisor)**2
-    A = (A//divisor)**2
-    C = (C//divisor)**2
-    G = (G//divisor)**2
-    I = (I//divisor)**2
-    B = 3*E - A - C
-    D = 3*E - A - G
-    F = 3*E - C - I
-    H = 3*E - G - I
-    add_solution([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
-
-
-
-
-    E = abs((u1**2 + v1**2)*(u2**2 + v2**2))
-    B = abs((u1**2 + v1**2)*(u2**2 + 2*u2*v2 - v2**2))
-    D = abs((u1**2 + 2*u1*v1 - v1**2)*(u2**2 + v2**2))
-    F = abs((- u1**2 + 2*u1*v1 + v1**2)*(u2**2 + v2**2))
-    H = abs((u1**2 + v1**2)*(- u2**2 + 2*u2*v2 + v2**2))
-
-    divisor = gcd_array([B,D,E,F,H])
-    E = (E//divisor)**2
-    B = (B//divisor)**2
-    D = (D//divisor)**2
-    F = (F//divisor)**2
-    H = (H//divisor)**2
-    A = (F + H)//2
-    C = (D + H)//2
-    G = (B + F)//2
-    I = (B + D)//2
-    add_solution([A,B,C,D,E,F,G,H,I],u1,v1,u2,v2)
 
 
 def add_all_labels(upper_bound):
@@ -150,6 +145,12 @@ def add_all_labels(upper_bound):
         six_squares.append(0)
         seven_squares.append(0)
 
+
+def create_progress_bar():
+    print("START |",str(datetime.now()))
+    print("END |",str(datetime.now()),"\n")
+
+    
 
 def get_pairs():
     return relatively_prime
@@ -166,6 +167,16 @@ def add_all_pairs(upper_bound):
     relatively_prime.sort()
 
 
+def align(data,length):
+    return (length - len(str(data)))*" " + str(data)
+
+
+
+def seconds_to_time(sec):
+    answer = str(sec//3600) + "h "
+    answer += str((sec%3600)//60) + "m "
+    answer += str((sec%3600)%60) + "s"
+    return answer
 
 
 
@@ -176,7 +187,7 @@ def run_all_pairs():
     allsteps = n*(n-1)//2
     step = 0
     
-    print("START |",str(datetime.now()))
+    print("\nSTART |",str(datetime.now()))
 
     for i1 in range(n):
         u1 = relatively_prime[i1][0]
@@ -187,7 +198,9 @@ def run_all_pairs():
             run_configuration(u1,v1,u2,v2)
             step += 1
             if step/allsteps * 100 > progress:
-                print(progress,"% |",step,"out of",allsteps)
+                print(align(progress,3),"% |",align(step,len(str(allsteps))),"out of",allsteps,
+                      "| estimated remaining time:",
+                      align(seconds_to_time((allsteps-step)//9300),12))
                 progress += 5
                 
     print("END |",str(datetime.now()),"\n")
@@ -213,11 +226,20 @@ def show_graph():
 
 
 
+
 def random_relatively_prime(upper_bound):
     a = random.randint(1,upper_bound-1)
     b = random.randint(1,upper_bound-1)
     d = math.gcd(a,b)
     return [a//d,b//d]
+
+
+
+
+def fixed_search(upper_bound):
+    add_all_pairs(upper_bound)
+    cProfile.run('run_all_pairs()')
+    show_graph()
 
 
 
@@ -227,61 +249,56 @@ def random_search(upper_bound,k):
         first_pair = random_relatively_prime(upper_bound)
         second_pair = random_relatively_prime(upper_bound)
         run_configuration(first_pair[0],first_pair[1],second_pair[0],second_pair[1])
+        
+
     
+
+def get_input(text,answers,error):
+    while True:
+        print(text)
+        answer = input()
+        if answer in answers: return answer
+        print(error)
+        
+
+
+
+def get_int_input(text,error):
+    while True:
+        print(text)
+        try:
+            upper_bound = int(input())
+            return upper_bound
+        except ValueError: print(error)
+        
+
 
 
 def main():
-    
-    while True:
-        print("Type 'fixed' to search all solutions.")
-        print("Type 'random' to search random solutions.")
-        answer = input()
-        if answer == "fixed":
-            search_fixed = True
-            break
-        
-        elif answer == "random":
-            search_fixed = False
-            break
-        
-        else: print("Invalid command:",answer,"\n")
-            
-    
-    while True:
-        try:
-            print("\nChoose a positive integer - upper bound for solutions:")
-            print("Tip: fixed search for upper bound above 70 may run for a very long time.")
-            print("Tip: fixed search for upper bound above 200 is currently out of reach.")
-                
-            answer = input()
-            upper_bound = int(answer)
-            break
-        
-        except ValueError: print("Invalid positive integer",answer)
-        
+    search_fixed = False
+    search = get_input("Type 'bruteforce' to search all solutions.\n" +
+                       "Type 'random' to search random solutions.\n" +
+                       "Type 'advanced' to search solutions using simulated annealing.\n",
+                       ["bruteforce","random","advanced"],"Invalid command.\n")
 
-    if search_fixed:
-        add_all_pairs(upper_bound)
-        cProfile.run('run_all_pairs()')
-        show_graph()
+    upper_bound = get_int_input("\nChoose a positive integer - upper bound for solutions:\n" +
+                                "Tip: bruteforce search for upper bound above 100 may run for a very long time.\n" +
+                                "Tip: bruteforce search for upper bound above 200 is currently out of reach.",
+                                "Invalid positive integer.\n")     
 
-    else:
-        while True:
-            try:
-                print("\nChoose a positive integer - number of iterations:")      
-                answer = input()
-                k = int(answer)
-                break
-        
-            except ValueError: print("Invalid positive integer",answer)
-     
-        random_search(upper_bound,k)
+    if search == "bruteforce": fixed_search(upper_bound)
+
+    elif search == "random": random_search(upper_bound,
+        get_int_input("\nChoose a positive integer - number of iterations:","Invalid positive integer.\n"))
+    
 
     print("")
-    print(len(solutions) - len(correct_solutions),"solutions with six squares were found.")
-    print(len(correct_solutions),"solutions with at least seven squares were found.")
-    for i in correct_solutions: print(i)
-
+    print(len(solutions) - len(correct_solutions),"solutions with six squares.")
+    print(len(correct_solutions),"solutions with at least seven squares:")
+    for i in correct_solutions:
+        print(" ".join(map(str,i[0])))
+        print("where u1 = " + str(i[1]) + ", v1 = " + str(i[2]) + ", u2 = " + str(i[3]) + ", v2 = " + str(i[4]))
+    input("\nPress any key to end.")
 
 
 
--- rocnikovy_projekt_test.py
+++ rocnikovy_projekt_test.py
@@ -2,8 +2,9 @@ import rocnikovy_projekt
 import pytest
 import random
 import math
+import datetime
 
-iterations = 5000
+iterations = 1000
 square_range = 10**12
 relatively_prime_range = 100
 
@@ -54,6 +55,86 @@ def test_relatively_prime_random_not_relatively_prime():
         a = d*random.randint(1,relatively_prime_range//d)
         b = d*random.randint(1,relatively_prime_range//d)
         assert [a,b] not in rocnikovy_projekt.get_pairs()
+
+
+
+def test_evaluate_simple_square():
+    solution = [2,7,6,
+                9,5,1,
+                4,3,8]
+    
+    assert rocnikovy_projekt.evaluate_square([2,    None, 6,
+                                              None, 5,    None,
+                                              4,    None, 8]) == solution
+    
+    assert rocnikovy_projekt.evaluate_square([2,    7,    None,
+                                              None, 5,    None,
+                                              None, 3,    8]) == solution
+    
+    assert rocnikovy_projekt.evaluate_square([None, 7,    None,
+                                              9,    5,    1,
+                                              None, 3,    None]) == solution
+
+    
+
+def test_evaluate_seven_squares_square():
+    solution = [42025,  277729, 222121,
+                360721, 180625, 529,
+                139129, 83521,  319225]
+    
+    assert rocnikovy_projekt.evaluate_square([42025,  None,   222121,
+                                              None,   180625, None,
+                                              139129, None,   319225]) == solution
+    
+    assert rocnikovy_projekt.evaluate_square([42025,  277729, None,
+                                              None,   180625, None,
+                                              None,   83521,  319225]) == solution
+    
+    assert rocnikovy_projekt.evaluate_square([None,   277729, None,
+                                              360721, 180625, 529,
+                                              None,   83521,  None]) == solution
+
+    
+
+def test_evaluate_random_acegi_square():
+    for _ in range(iterations):
+        x = random.randint(1,square_range)
+        y = random.randint(1,square_range)
+        z = random.randint(1,square_range)
+
+        solution = [2*x+1,          6*z-2*x-2*y+1,  2*y+1,
+                    2*z-2*x+2*y+1,  2*z+1,          2*z+2*x-2*y+1,
+                    4*z-2*y+1,      2*x+2*y-2*z+1,  4*z-2*x+1]
+        
+        assert rocnikovy_projekt.evaluate_square([2*x+1,None,2*y+1,None,2*z+1,None,4*z-2*y+1,None,4*z-2*x+1]) == solution
+
+
+def test_evaluate_random_abehi_square():
+    for _ in range(iterations):
+        x = random.randint(1,square_range)
+        y = random.randint(1,square_range)
+        z = random.randint(1,square_range)
+
+        solution = [2*x+1,          2*y+1,          6*z-2*x-2*y+1,
+                    8*z-4*x-2*y+1,  2*z+1,          4*x+2*y-4*z+1,
+                    2*x+2*y-2*z+1,  4*z-2*y+1,      4*z-2*x+1]
+        
+        assert rocnikovy_projekt.evaluate_square([2*x+1,2*y+1,None,None,2*z+1,None,None,4*z-2*y+1,4*z-2*x+1]) == solution
+
+
+
+def test_evaluate_random_bdefh_square():
+    for _ in range(3):
+        x = random.randint(1,square_range)
+        y = random.randint(1,square_range)
+        z = random.randint(1,square_range)
+
+        solution = [4*z-x-y+1,      2*x+1,          2*z-x+y+1,
+                    2*y+1,          2*z+1,          4*z-2*y+1,
+                    2*z+x-y+1,      4*z-2*x+1,      x+y+1]
+        
+        assert rocnikovy_projekt.evaluate_square([None,2*x+1,None,2*y+1,2*z+1,4*z-2*y+1,None,4*z-2*x+1,None]) == solution
+    
 
 
 
