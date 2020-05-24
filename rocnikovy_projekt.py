fdff3d4da0f7b81ab56876c1deac0b51fe0c4937
Richard Biro <biro.risko@gmail.com>
2020-05-24 22:24:27 +0200

    unittest namiesto pytestu, pridany zapis do logu (odolny voci vypadkom), refaktorizacia kodu

--- rocnikovy_projekt.py
+++ rocnikovy_projekt.py
@@ -1,10 +1,18 @@
-import math
-import numpy as np
-import matplotlib.pyplot as plt
-from datetime import datetime
-import random
 import cProfile
+from datetime import datetime
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import multiprocessing as mp
 
+file = str(datetime.now())
+file = file[:file.index(".")]
+file = file.replace(":", "-")
+file = file + ".txt"
+
+buffer = ""
+timestamp = datetime.now()
 solutions = dict()
 correct_solutions = []
 relatively_prime = []
@@ -12,103 +20,116 @@ labels = []
 six_squares = []
 seven_squares = []
 
-
 def gcd_array(array):
-    if len(array) < 2: return None
-    d = math.gcd(array[0],array[1])
-    for i in range(2,len(array)): d = math.gcd(d,array[i])
+    if len(array) < 2:
+        return None
+    d = math.gcd(array[0], array[1])
+    for i in range(2, len(array)): d = math.gcd(d, array[i])
     return d
 
-
 def is_square(n):
-    if n < 0: return False
-    return (int(math.sqrt(n))**2 == n)
-
+    if n < 0:
+        return False
+    return int(math.isqrt(n))**2 == n
 
 def get_solutions():
     return solutions
 
+def write_buffer():
+    global buffer
+    with open(file, 'a') as f:
+        f.write(buffer)
+    buffer = ""
 
-def add_correct_solution(array,u1,v1,u2,v2):
-    if [array,u1,v1,u2,v2] not in correct_solutions:
-        correct_solutions.append([array,u1,v1,u2,v2])
-        
+def check_time():
+    global timestamp
+    timestamp2 = datetime.now()
+    delta = timestamp2 - timestamp
+    if delta.seconds > 60:
+        timestamp = timestamp2
+        write_buffer()
 
-def add_solution(array,u1,v1,u2,v2):
+def add_correct_solution(array, u1, v1, u2, v2):
+    if [array, u1, v1, u2, v2] not in correct_solutions:
+        correct_solutions.append([array, u1, v1, u2, v2])
+
+def add_solution(array, u1, v1, u2, v2):
+    global buffer
     different = len(set(array))
 
     if different == 9:
         squares = 0
         for i in array:
-            if is_square(i): squares += 1
+            if is_square(i):
+                squares += 1
 
         if squares > 5:
-            corners = [array[0],array[2],array[6],array[8]]
+            corners = [array[0], array[2], array[6], array[8]]
             corners.sort()
             corner1 = corners[0]
             corner2 = corners[1]
             center = array[4]
-            
-            index = ' '.join(str(s) for s in [corner1,corner2,center])
-            largest = max(u1,v1,u2,v2)
+            index = ' '.join(str(s) for s in [corner1, corner2, center])
+            largest = max(u1, v1, u2, v2)
 
-            if index not in solutions or list(map(int,solutions[index].split(' ')))[0] > largest:
-                solutions[index] = ' '.join(str(s) for s in [largest,squares])
+            if (index not in solutions or
+                    list(map(int, solutions[index].split(' ')))[0] > largest):
+                buffer += str(str(squares) + " squares | u1 = " + str(u1) +
+                              " | v1 = " + str(v1) + " | u2 = " + str(u2) +
+                              " | v2 = " + str(v2) + " | " + str(array) + "\n")
+                solutions[index] = ' '.join(str(s) for s in [largest, squares])
                 center = int(math.sqrt(center))
 
-                if squares == 6: six_squares[largest] += 1
-                        
+                if squares == 6:
+                    six_squares[largest] += 1
                 else:
                     seven_squares[largest] += 1
-                    add_correct_solution(array,u1,v1,u2,v2)
-
+                    add_correct_solution(array, u1, v1, u2, v2)
 
 def evaluate_rows(array):
     for i in range(3):
         for j in range(3):
-            if array[3*i+j%3] != None and array[3*i+(j+1)%3] != None and array[3*i+(j+2)%3] == None:
-                    array[3*i+(j+2)%3] = 3*array[4] - array[3*i+j%3] - array[3*i+(j+1)%3]
-
+            if (array[3*i+j%3] is not None and
+                    array[3*i+(j+1)%3] is not None and
+                    array[3*i+(j+2)%3] is None):
+                array[3*i+(j+2)%3] = 3*array[4] - array[3*i+j%3] - array[3*i+(j+1)%3]
 
 def evaluate_columns(array):
     for i in range(3):
         for j in range(3):
-            if array[i+3*(j%3)] != None and array[i+3*((j+1)%3)] != None and array[i+3*((j+2)%3)] == None:
+            if (array[i+3*(j%3)] is not None and
+                    array[i+3*((j+1)%3)] is not None and
+                    array[i+3*((j+2)%3)] is None):
                 array[i+3*((j+2)%3)] = 3*array[4] - array[i+3*(j%3)] - array[i+3*((j+1)%3)]
-
-
 
 def evaluate_two_edges(array):
     for i in range(2):
         for j in range(2):
-            if array[1+6*i] != None and array[3+2*j] != None:
+            if (array[1+6*i] is not None and
+                    array[3+2*j] is not None):
                 array[8-6*i-2*j] = (array[1+6*i] + array[3+2*j])//2
-
 
 def evaluate_square(array):
     evaluate_rows(array)
     evaluate_columns(array)
     evaluate_two_edges(array)
 
-    if None in array: return None
+    if None in array:
+        return None
     return array
 
+def normalize(array, divisor):
+    for i in range(len(array)):
+        array[i] = (array[i]//divisor)**2
 
-
-def normalize(array,divisor):
-    for i in range(len(array)): array[i] = (array[i]//divisor)**2
-
-
-
-def run_configuration(u1,v1,u2,v2):
+def run_configuration(u1, v1, u2, v2):
     constants = [abs((u1**2 + v1**2)*(u2**2 + v2**2)),
                  abs((u1**2 + v1**2)*(u2**2 + 2*u2*v2 - v2**2)),
                  abs((u1**2 + v1**2)*(- u2**2 + 2*u2*v2 + v2**2)),
                  abs((u1**2 + 2*u1*v1 - v1**2)*(u2**2 + v2**2)),
                  abs((- u1**2 + 2*u1*v1 + v1**2)*(u2**2 + v2**2))]
-    
     divisor = gcd_array(constants)
-    normalize(constants,divisor)
+    normalize(constants, divisor)
 
     center = constants[0]
     x1 = constants[1]
@@ -117,58 +138,46 @@ def run_configuration(u1,v1,u2,v2):
     y2 = constants[4]
     squares = []
 
-    squares.append(evaluate_square([x1,   y1,     None,
+    squares.append(evaluate_square([x1, y1, None,
                                     None, center, None,
-                                    None, y2,     x2]))
+                                    None, y2, x2]))
 
-    squares.append(evaluate_square([x1,   y2,     None,
+    squares.append(evaluate_square([x1, y2, None,
                                     None, center, None,
-                                    None, y1,     x2]))
+                                    None, y1, x2]))
 
-    squares.append(evaluate_square([x1,   None,   y1,
+    squares.append(evaluate_square([x1, None, y1,
                                     None, center, None,
-                                    y2,   None,   x2]))
+                                    y2, None, x2]))
 
-    squares.append(evaluate_square([None, x1,     None,
-                                    y1,   center, y2,
-                                    None, x2,     None]))
-    
+    squares.append(evaluate_square([None, x1, None,
+                                    y1, center, y2,
+                                    None, x2, None]))
     for i in squares:
-        if i != None: add_solution(i,u1,v1,u2,v2)
-
-
-
+        if i is not None:
+            add_solution(i, u1, v1, u2, v2)
 
 def add_all_labels(upper_bound):
     for i in range(upper_bound+1):
         labels.append(i)
         six_squares.append(0)
         seven_squares.append(0)
-    
-    
-    
-
+        
 def get_pairs():
     return relatively_prime
-
-
 
 def add_all_pairs(upper_bound):
     add_all_labels(upper_bound)
     
-    for i in range(1,upper_bound+1):
-        for j in range(1,upper_bound+1):
-            if math.gcd(i,j) == 1:
-                relatively_prime.append([i,j])
+    for i in range(1, upper_bound+1):
+        for j in range(1, upper_bound+1):
+            if math.gcd(i, j) == 1:
+                relatively_prime.append([i, j])
 
     relatively_prime.sort()
 
-
-
-def align(data,length):
+def align(data, length):
     return (length - len(str(data)))*" " + str(data)
-
-
 
 def seconds_to_time(sec):
     answer = str(sec//3600) + "h "
@@ -176,43 +185,38 @@ def seconds_to_time(sec):
     answer += str((sec%3600)%60) + "s"
     return answer
 
-
-
 def run_all_pairs():
     progress = 0
     n = len(relatively_prime)
     allsteps = n*(n-1)//2
     step = 0
     
-    print("\nSTART |",str(datetime.now()))
-
+    print("\nSTART |", str(datetime.now()))
     for i1 in range(n):
         u1 = relatively_prime[i1][0]
         v1 = relatively_prime[i1][1]
-        for i2 in range(i1+1,n):
+        for i2 in range(i1+1, n):
             u2 = relatively_prime[i2][0]
             v2 = relatively_prime[i2][1]
-            run_configuration(u1,v1,u2,v2)
+            run_configuration(u1, v1, u2, v2)
             step += 1
             
             if step/allsteps * 100 > progress:
-                print(align(progress,3),"% |",align(step,len(str(allsteps))),"out of",allsteps,
-                      "| estimated remaining time:",
-                      align(seconds_to_time((allsteps-step)//9300),12))
+                print(align(progress, 3), "% |", align(step, len(str(allsteps))),
+                      "out of", allsteps, "| estimated remaining time:",
+                      align(seconds_to_time((allsteps-step)//9300), 12))
                 progress += 5
-                
-    print("END |",str(datetime.now()),"\n")
-                
-
-
+            check_time()
+    write_buffer()
+    print("END |", str(datetime.now()), "\n")
 
 def show_graph():
     x = np.arange(len(labels))
     width = 0.35
 
     fig, ax = plt.subplots()
-    rects1 = ax.bar(x - width/2, six_squares, width, label='exactly 6 entries are squares')
-    rects2 = ax.bar(x + width/2, seven_squares, width, label='at least 7 entries are squares')
+    ax.bar(x - width/2, six_squares, width, label='exactly 6 entries are squares')
+    ax.bar(x + width/2, seven_squares, width, label='at least 7 entries are squares')
 
     ax.set_xlabel('Maximum from u1, v1, u2, v2')
     ax.set_xticks(x)
@@ -222,83 +226,73 @@ def show_graph():
     fig.tight_layout()
     plt.show()
 
-
-
-
 def random_relatively_prime(upper_bound):
-    a = random.randint(1,upper_bound-1)
-    b = random.randint(1,upper_bound-1)
-    d = math.gcd(a,b)
-    return [a//d,b//d]
-
-
-
+    first = random.randint(1, upper_bound-1)
+    second = random.randint(1, upper_bound-1)
+    divisor = math.gcd(first, second)
+    return [first//divisor, second//divisor]
 
 def fixed_search(upper_bound):
     add_all_pairs(upper_bound)
     cProfile.run('run_all_pairs()')
     show_graph()
 
-
-
-
-def random_search(upper_bound,k):
+def random_search(upper_bound, k):
     add_all_labels(upper_bound)
     for _ in range(k):
         first_pair = random_relatively_prime(upper_bound)
         second_pair = random_relatively_prime(upper_bound)
-        run_configuration(first_pair[0],first_pair[1],second_pair[0],second_pair[1])
+        run_configuration(first_pair[0], first_pair[1],
+                          second_pair[0], second_pair[1])
     show_graph()
-        
 
-    
-
-def get_input(text,answers,error):
+def get_input(text, answers, error):
     while True:
         print(text)
         answer = input()
-        if answer in answers: return answer
+        if answer in answers:
+            return answer
         print(error)
-        
 
-
-
-def get_int_input(text,error):
+def get_int_input(text, error):
     while True:
         print(text)
         try:
             upper_bound = int(input())
             return upper_bound
-        except ValueError: print(error)
-        
-
-
+        except ValueError:
+            print(error)
 
 def main():
-    search_fixed = False
     search = get_input("Type 'bruteforce' to search all solutions.\n" +
                        "Type 'random' to search random solutions.\n",
-                       ["bruteforce","random"],"Invalid command.\n")
+                       ["bruteforce", "random"], "Invalid command.\n")
 
     upper_bound = get_int_input("\nChoose a positive integer - upper bound for solutions:\n" +
-                                "Tip: bruteforce search for upper bound above 100 may run for a very long time.\n" +
-                                "Tip: bruteforce search for upper bound above 300 is currently out of reach.",
-                                "Invalid positive integer.\n")     
+                                "Tip: bruteforce search for upper bound > 100 may take long.\n" +
+                                "Tip: bruteforce search for upper bound > 300 is out of reach.",
+                                "Invalid positive integer.\n")
 
-    if search == "bruteforce": fixed_search(upper_bound)
+    with open(file, 'w') as f:
+        f.write("")
 
-    elif search == "random": random_search(upper_bound,
-        get_int_input("\nChoose a positive integer - number of iterations:","Invalid positive integer.\n"))
+    if search == "bruteforce":
+        fixed_search(upper_bound)
+    elif search == "random":
+        random_search(upper_bound,
+                      get_int_input("\nChoose a positive integer - number of iterations:",
+                                    "Invalid positive integer.\n"))
     
 
-    print("\n" + str(len(solutions) - len(correct_solutions)),"solutions with six squares.\n")
-    print(len(correct_solutions),"solutions with at least seven squares:")
+    print("\n" + str(len(solutions) - len(correct_solutions)), "solutions with six squares.\n")
+    print(len(correct_solutions), "solutions with at least seven squares:")
     for i in correct_solutions:
-        print(" ".join(map(str,i[0])))
-        print("where u1 = " + str(i[1]) + ", v1 = " + str(i[2]) + ", u2 = " + str(i[3]) + ", v2 = " + str(i[4]) + "\n")
+        print(" ".join(map(str, i[0])))
+        print("| u1 =", i[1],
+              "| v1 =", i[2],
+              "| u2 =", i[3],
+              "| v2 =", i[4], "|\n")
     input("\nPress any key to end.")
-
-
 
 if __name__ == '__main__':
     main()
--- rocnikovy_projekt_test.py
+++ rocnikovy_projekt_test.py
@@ -1,141 +1,149 @@
-import rocnikovy_projekt
-import pytest
-import random
-import math
-import datetime
-
-iterations = 1000
-square_range = 10**12
-relatively_prime_range = 100
+from math import gcd, sqrt
+from random import randint
+from rocnikovy_projekt import is_square, add_all_pairs, get_pairs, evaluate_square
+from unittest import main, TestCase
 
 
-def test_square_random_positive_square():
-    for _ in range(iterations):
-        n = random.randint(1,square_range)
-        assert rocnikovy_projekt.is_square(n**2) is True
-
-
-def test_square_random_positive_not_square():
-    for _ in range(iterations):
-        m = random.randint(2,square_range)
-        n = random.randint(m**2 + 1, (m + 1)**2 - 1)
-        assert rocnikovy_projekt.is_square(n) is False
-
-
-def test_square_random_negative():
-    for _ in range(iterations):
-        n = - random.randint(1,square_range)
-        assert rocnikovy_projekt.is_square(n) is False
-
-
-def test_square_zero():
-    assert rocnikovy_projekt.is_square(0) is True
-
-
-def test_relatively_prime_one_one_relatively_prime():
-    rocnikovy_projekt.add_all_pairs(1)
-    assert [1,1] in rocnikovy_projekt.get_pairs()
-
-
-def test_relatively_prime_random_relatively_prime():
-    rocnikovy_projekt.add_all_pairs(relatively_prime_range+1)
-    for _ in range(iterations):
-        a = random.randint(1,relatively_prime_range)
-        b = random.randint(1,relatively_prime_range)
-        d = math.gcd(a,b)
-        a = a//d
-        b = b//d
-        assert [a,b] in rocnikovy_projekt.get_pairs()
-
-
-def test_relatively_prime_random_not_relatively_prime():
-    rocnikovy_projekt.add_all_pairs(relatively_prime_range+1)
-    for _ in range(iterations):
-        d = random.randint(2,int(math.sqrt(relatively_prime_range)))
-        a = d*random.randint(1,relatively_prime_range//d)
-        b = d*random.randint(1,relatively_prime_range//d)
-        assert [a,b] not in rocnikovy_projekt.get_pairs()
-
-
-
-def test_evaluate_simple_square():
-    solution = [2,7,6,
-                9,5,1,
-                4,3,8]
-    
-    assert rocnikovy_projekt.evaluate_square([2,    None, 6,
-                                              None, 5,    None,
-                                              4,    None, 8]) == solution
-    
-    assert rocnikovy_projekt.evaluate_square([2,    7,    None,
-                                              None, 5,    None,
-                                              None, 3,    8]) == solution
-    
-    assert rocnikovy_projekt.evaluate_square([None, 7,    None,
-                                              9,    5,    1,
-                                              None, 3,    None]) == solution
-
-    
-
-def test_evaluate_seven_squares_square():
-    solution = [42025,  277729, 222121,
-                360721, 180625, 529,
-                139129, 83521,  319225]
-    
-    assert rocnikovy_projekt.evaluate_square([42025,  None,   222121,
-                                              None,   180625, None,
-                                              139129, None,   319225]) == solution
-    
-    assert rocnikovy_projekt.evaluate_square([42025,  277729, None,
-                                              None,   180625, None,
-                                              None,   83521,  319225]) == solution
-    
-    assert rocnikovy_projekt.evaluate_square([None,   277729, None,
-                                              360721, 180625, 529,
-                                              None,   83521,  None]) == solution
-
-    
-
-def test_evaluate_random_acegi_square():
-    for _ in range(iterations):
-        x = random.randint(1,square_range)
-        y = random.randint(1,square_range)
-        z = random.randint(1,square_range)
-
-        solution = [2*x+1,          6*z-2*x-2*y+1,  2*y+1,
-                    2*z-2*x+2*y+1,  2*z+1,          2*z+2*x-2*y+1,
-                    4*z-2*y+1,      2*x+2*y-2*z+1,  4*z-2*x+1]
+class SquareTest(TestCase):
+    def setUp(self):
+        self.iterations = 1000
+        self.square_range = 10**12
+        self.relatively_prime_range = 100
         
-        assert rocnikovy_projekt.evaluate_square([2*x+1,None,2*y+1,None,2*z+1,None,4*z-2*y+1,None,4*z-2*x+1]) == solution
+    def test_square_random_positive_square(self):
+        for _ in range(self.iterations):
+            n = randint(1, self.square_range)
+            self.assertTrue(is_square(n**2))
 
 
-def test_evaluate_random_abehi_square():
-    for _ in range(iterations):
-        x = random.randint(1,square_range)
-        y = random.randint(1,square_range)
-        z = random.randint(1,square_range)
+    def test_square_random_positive_not_square(self):
+        for _ in range(self.iterations):
+            m = randint(2, self.square_range)
+            n = randint(m**2 + 1, (m + 1)**2 - 1)
+            self.assertFalse(is_square(n))
 
-        solution = [2*x+1,          2*y+1,          6*z-2*x-2*y+1,
-                    8*z-4*x-2*y+1,  2*z+1,          4*x+2*y-4*z+1,
-                    2*x+2*y-2*z+1,  4*z-2*y+1,      4*z-2*x+1]
+
+    def test_square_random_negative(self):
+        for _ in range(self.iterations):
+            n = - randint(1, self.square_range)
+            self.assertFalse(is_square(n))
+
+
+    def test_square_zero(self):
+        self.assertTrue(is_square(0))
+
+
+    def test_relatively_prime_one_one_relatively_prime(self):
+        add_all_pairs(1)
+        self.assertTrue([1, 1] in get_pairs())
+
+
+    def test_relatively_prime_random_relatively_prime(self):
+        add_all_pairs(self.relatively_prime_range+1)
+        for _ in range(self.iterations):
+            a = randint(1, self.relatively_prime_range)
+            b = randint(1, self.relatively_prime_range)
+            d = gcd(a, b)
+            a = a//d
+            b = b//d
+            self.assertTrue([a, b] in get_pairs())
+
+
+    def test_relatively_prime_random_not_relatively_prime(self):
+        add_all_pairs(self.relatively_prime_range+1)
+        for _ in range(self.iterations):
+            d = randint(2, int(sqrt(self.relatively_prime_range)))
+            a = d*randint(1, self.relatively_prime_range//d)
+            b = d*randint(1, self.relatively_prime_range//d)
+            self.assertTrue([a, b] not in get_pairs())
+
+
+
+    def test_evaluate_simple_square(self):
+        solution = [2, 7, 6,
+                    9, 5, 1,
+                    4, 3, 8]
         
-        assert rocnikovy_projekt.evaluate_square([2*x+1,2*y+1,None,None,2*z+1,None,None,4*z-2*y+1,4*z-2*x+1]) == solution
-
-
-
-def test_evaluate_random_bdefh_square():
-    for _ in range(3):
-        x = random.randint(1,square_range)
-        y = random.randint(1,square_range)
-        z = random.randint(1,square_range)
-
-        solution = [4*z-x-y+1,      2*x+1,          2*z-x+y+1,
-                    2*y+1,          2*z+1,          4*z-2*y+1,
-                    2*z+x-y+1,      4*z-2*x+1,      x+y+1]
+        self.assertTrue(evaluate_square([2, None, 6,
+                                         None, 5, None,
+                                         4, None, 8]) == solution)
         
-        assert rocnikovy_projekt.evaluate_square([None,2*x+1,None,2*y+1,2*z+1,4*z-2*y+1,None,4*z-2*x+1,None]) == solution
+        self.assertTrue(evaluate_square([2, 7, None,
+                                         None, 5, None,
+                                         None, 3, 8]) == solution)
+        
+        self.assertTrue(evaluate_square([None, 7, None,
+                                         9, 5, 1,
+                                         None, 3, None]) == solution)
+
+        
+
+    def test_evaluate_seven_squares_square(self):
+        solution = [42025, 277729, 222121,
+                    360721, 180625, 529,
+                    139129, 83521, 319225]
+        
+        self.assertTrue(evaluate_square([42025, None, 222121,
+                                         None, 180625, None,
+                                         139129, None, 319225]) == solution)
+        
+        self.assertTrue(evaluate_square([42025, 277729, None,
+                                         None, 180625, None,
+                                         None, 83521, 319225]) == solution)
+        
+        self.assertTrue(evaluate_square([None, 277729, None,
+                                         360721, 180625, 529,
+                                         None, 83521, None]) == solution)
+
+        
+
+    def test_evaluate_random_acegi_square(self):
+        for _ in range(self.iterations):
+            x = randint(1, self.square_range)
+            y = randint(1, self.square_range)
+            z = randint(1, self.square_range)
+
+            solution = [2*x+1, 6*z-2*x-2*y+1, 2*y+1,
+                        2*z-2*x+2*y+1, 2*z+1, 2*z+2*x-2*y+1,
+                        4*z-2*y+1, 2*x+2*y-2*z+1, 4*z-2*x+1]
+            
+            self.assertTrue(evaluate_square([2*x+1, None, 2*y+1,
+                                             None, 2*z+1, None,
+                                             4*z-2*y+1, None, 4*z-2*x+1]) == solution)
+
+
+    def test_evaluate_random_abehi_square(self):
+        for _ in range(self.iterations):
+            x = randint(1, self.square_range)
+            y = randint(1, self.square_range)
+            z = randint(1, self.square_range)
+
+            solution = [2*x+1, 2*y+1, 6*z-2*x-2*y+1,
+                        8*z-4*x-2*y+1, 2*z+1, 4*x+2*y-4*z+1,
+                        2*x+2*y-2*z+1, 4*z-2*y+1, 4*z-2*x+1]
+            
+            self.assertTrue(evaluate_square([2*x+1, 2*y+1, None,
+                                             None, 2*z+1, None,
+                                             None, 4*z-2*y+1, 4*z-2*x+1]) == solution)
+
+
+
+    def test_evaluate_random_bdefh_square(self):
+        for _ in range(self.iterations):
+            x = randint(1, self.square_range)
+            y = randint(1, self.square_range)
+            z = randint(1, self.square_range)
+
+            solution = [4*z-x-y+1, 2*x+1, 2*z-x+y+1,
+                        2*y+1, 2*z+1, 4*z-2*y+1,
+                        2*z+x-y+1, 4*z-2*x+1, x+y+1]
+            
+            self.assertTrue(evaluate_square([None, 2*x+1, None,
+                                             2*y+1, 2*z+1, 4*z-2*y+1,
+                                             None, 4*z-2*x+1, None]) == solution)
     
 
 
-
+if __name__ == '__main__':
+    main()
 
