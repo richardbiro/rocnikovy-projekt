8bb35780f210c3e948f3dd75b1304035c06a7436
Richard Biro <biro.risko@gmail.com>
2020-05-29 20:39:34 +0200

    pridany multiprocessing, prehladane pre u1, v1, u2, v2 <= 500

--- rocnikovy_projekt.py
+++ rocnikovy_projekt.py
@@ -1,18 +1,17 @@
-import cProfile
+from cProfile import runctx
 from datetime import datetime
-import math
+from math import gcd, isqrt
 import matplotlib.pyplot as plt
-import numpy as np
-import random
+from numpy import arange
+from random import randint
 from multiprocessing import Pool
+from itertools import combinations
 
 file = str(datetime.now())
 file = file[:file.index(".")]
 file = file.replace(":", "-")
 file = file + ".txt"
 
-buffer = ""
-timestamp = datetime.now()
 solutions = dict()
 correct_solutions = []
 relatively_prime = []
@@ -23,14 +22,14 @@ seven_squares = []
 def gcd_array(array):
     if len(array) < 2:
         return None
-    d = math.gcd(array[0], array[1])
-    for i in range(2, len(array)): d = math.gcd(d, array[i])
+    d = gcd(array[0], array[1])
+    for i in range(2, len(array)): d = gcd(d, array[i])
     return d
 
 def is_square(n):
     if n < 0:
         return False
-    return int(math.isqrt(n))**2 == n
+    return isqrt(n)**2 == n
 
 def get_pairs():
     return relatively_prime
@@ -44,7 +43,7 @@ def add_all_labels(upper_bound):
 def add_all_pairs(upper_bound):
     for i in range(1, upper_bound+1):
         for j in range(i, upper_bound+1):
-            if math.gcd(i, j) == 1:
+            if gcd(i, j) == 1:
                 relatively_prime.append([max(i, j), i, j])
     relatively_prime.sort()
     for i in range(len(relatively_prime)):
@@ -53,62 +52,49 @@ def add_all_pairs(upper_bound):
 def get_solutions():
     return solutions
 
-def write_buffer():
+def get_index(array):
+    corners = [array[0], array[2], array[6], array[8]]
+    corners.sort()
+    corner1 = corners[0]
+    corner2 = corners[1]
+    center = array[4]
+    return ' '.join(str(s) for s in [corner1, corner2, center])
+
+def add_solution(array, uv):
     global buffer
-    with open(file, 'a') as f:
-        f.write(buffer)
-    buffer = ""
+    u1 = uv[0]
+    v1 = uv[1]
+    u2 = uv[2]
+    v2 = uv[3]
+    squares = array[0]
+    square = array[1]
+    index = get_index(square)
+    largest = max(u1, v1, u2, v2)
 
-def check_time():
-    global timestamp
-    timestamp2 = datetime.now()
-    delta = timestamp2 - timestamp
-    if delta.seconds > 60:
-        timestamp = timestamp2
-        write_buffer()
+    if (index not in solutions or
+            list(map(int, solutions[index].split(' ')))[0] > largest):
+        with open(file, 'a') as f:
+            f.write(str(str(squares) + " squares | u1 = " + str(u1) +
+                        " | v1 = " + str(v1) + " | u2 = " + str(u2) +
+                        " | v2 = " + str(v2) + " | " + str(square) + "\n"))
+        solutions[index] = ' '.join(str(s) for s in [largest, squares])
 
-def add_correct_solution(array, u1, v1, u2, v2):
-    if [array, u1, v1, u2, v2] not in correct_solutions:
-        correct_solutions.append([array, u1, v1, u2, v2])
-
-def add_solution(array, u1, v1, u2, v2):
-    global buffer
-    different = len(set(array))
-
-    if different == 9:
-        squares = 0
-        for i in array:
-            if is_square(i):
-                squares += 1
-
-        if squares > 5:
-            corners = [array[0], array[2], array[6], array[8]]
-            corners.sort()
-            corner1 = corners[0]
-            corner2 = corners[1]
-            center = array[4]
-            index = ' '.join(str(s) for s in [corner1, corner2, center])
-            largest = max(u1, v1, u2, v2)
-
-            if (index not in solutions or
-                    list(map(int, solutions[index].split(' ')))[0] > largest):
-                buffer += str(str(squares) + " squares | u1 = " + str(u1) +
-                              " | v1 = " + str(v1) + " | u2 = " + str(u2) +
-                              " | v2 = " + str(v2) + " | " + str(array) + "\n")
-                solutions[index] = ' '.join(str(s) for s in [largest, squares])
-                center = int(math.sqrt(center))
-
-                if squares == 6:
-                    six_squares[largest] += 1
-                else:
-                    seven_squares[largest] += 1
-                    add_correct_solution(array, u1, v1, u2, v2)
+        if squares == 6:
+            six_squares[largest] += 1
+        else:
+            seven_squares[largest] += 1
+            if [array, u1, v1, u2, v2] not in correct_solutions:
+                correct_solutions.append([array, u1, v1, u2, v2])
 
 def normalize(array, divisor):
     for i in range(len(array)):
         array[i] = (array[i]//divisor)**2
 
-def run_configuration(u1, v1, u2, v2):
+def run_configuration(uv):
+    u1 = uv[0]
+    v1 = uv[1]
+    u2 = uv[2]
+    v2 = uv[3]
     constants = [abs((u1**2 + v1**2)*(u2**2 + v2**2)),
                  abs((u1**2 + v1**2)*(u2**2 + 2*u2*v2 - v2**2)),
                  abs((u1**2 + v1**2)*(- u2**2 + 2*u2*v2 + v2**2)),
@@ -123,7 +109,6 @@ def run_configuration(u1, v1, u2, v2):
     y1 = constants[3]
     y2 = constants[4]
     squares = []
-
     squares.append([x1, y1, 3*center - x1 - y1,
                     x2 + y2 - x1, center, x1 + y1 - x2,
                     3*center - x2 - y2, y2, x2])
@@ -140,10 +125,24 @@ def run_configuration(u1, v1, u2, v2):
         squares.append([(x2 + y2)//2, x1, (x2 + y1)//2,
                         y1, center, y2,
                         (x1 + y2)//2, x2, (x1 + y1)//2])
-    
-    for i in squares:
-        add_solution(i, u1, v1, u2, v2)
-    check_time()
+    else:
+        squares.append([x2 + y2, 2*x1, x2 + y1,
+                        2*y1, 2*center, 2*y2,
+                        x1 + y2, 2*x2, x1 + y1])
+    answers = []
+    for S in squares:
+        if len(set(S)) < 9:
+            answers.append(None)
+        else:
+            counter = 0
+            for i in S:
+                if is_square(i):
+                    counter += 1
+            if counter <= 5:
+                answers.append(None)
+            else:
+                answers.append([counter, S])
+    return [answers, [u1, v1, u2, v2]]
 
 def align(data, length):
     return (length - len(str(data)))*" " + str(data)
@@ -155,33 +154,46 @@ def seconds_to_time(sec):
     return answer
 
 def run_all_pairs(lower_bound):
-    progress = 0
     n = len(relatively_prime)
-    step = 0
-    i = 0
-    while max(relatively_prime[i]) < lower_bound:
-        i += 1
-    allsteps = n*(n-1)//2 - i*(i-1)//2
-    
+    low = 0
+    while max(relatively_prime[low]) < lower_bound:
+        low += 1
+    allsteps = n*(n-1)//2 - low*(low-1)//2
+    execute_list = []
+    block = 10**6
+    blocks = allsteps//block + 1
+    completed = 0
     print("\nSTART |", str(datetime.now()))
+
     for i1 in range(n):
         u1 = relatively_prime[i1][0]
         v1 = relatively_prime[i1][1]
-        for i2 in range(max(i, i1+1), n):
+        for i2 in range(max(low, i1+1), n):
             u2 = relatively_prime[i2][0]
             v2 = relatively_prime[i2][1]
-            run_configuration(u1, v1, u2, v2)
-            step += 1
-            
-            if step/allsteps * 100 > progress:
-                print(align(progress, 3), "% |", align(step, len(str(allsteps))),
-                      "out of", allsteps, "| estimated remaining time:",
-                      align(seconds_to_time((allsteps-step)//16400), 12))
-                progress += 5
+            execute_list.append([u1, v1, u2, v2])
+            if len(execute_list) > block or (i1 == n-2 and i2 == n-1):
+                start = datetime.now()
+                p = Pool()
+                results = p.map(run_configuration, execute_list)
+                for i in range(len(results)):
+                    for S in results[i][0]:
+                        if S is not None:
+                            add_solution(S, results[i][1])
+                p.close()
+                p.join()
+                execute_list = []
+                completed += 1
+                end = datetime.now()
+                diff = end - start
+                print(align(int(100*(completed*block)/allsteps), 3), "% |",
+                      align(completed*block, len(str(allsteps))), "out of", allsteps,
+                      "| estimated remaining time:", align(seconds_to_time(
+                          int(diff.seconds*(allsteps - completed*block)/block)), 12))    
     print("END |", str(datetime.now()), "\n")
 
 def show_graph(lower_bound):
-    x = np.arange(len(labels[lower_bound:]))
+    x = arange(len(labels[lower_bound:]))
     width = 0.35
 
     fig, ax = plt.subplots()
@@ -197,23 +209,23 @@ def show_graph(lower_bound):
     plt.show()
 
 def random_relatively_prime(lower_bound, upper_bound):
-    first = random.randint(lower_bound, upper_bound-1)
-    second = random.randint(lower_bound, upper_bound-1)
-    divisor = math.gcd(first, second)
+    first = randint(lower_bound, upper_bound-1)
+    second = randint(lower_bound, upper_bound-1)
+    divisor = gcd(first, second)
     return [first//divisor, second//divisor]
 
 def fixed_search(lower_bound, upper_bound):
     add_all_pairs(upper_bound)
-    cProfile.runctx('r(l)', {'r': run_all_pairs}, {'l': lower_bound})
-    show_graph(lower_bound)
-
+    runctx('r(l)', {'r': run_all_pairs}, {'l': lower_bound})
+    
 def random_search(lower_bound, upper_bound, k):
     for _ in range(k):
         first_pair = random_relatively_prime(lower_bound, upper_bound)
         second_pair = random_relatively_prime(lower_bound, upper_bound)
-        run_configuration(first_pair[0], first_pair[1],
-                          second_pair[0], second_pair[1])
-    show_graph(lower_bound)
+        result = run_configuration(first_pair + second_pair)
+        for S in result[0]:
+            if S is not None:
+                add_solution(S, result[1])
 
 def get_input(text, answers, error):
     while True:
@@ -241,12 +253,12 @@ def main():
                                 "Invalid positive integer.\n")
 
     upper_bound = get_int_input("\nChoose a positive integer - upper bound for solutions:\n" +
-                                "Tip: bruteforce search for upper bound > 100 may take long.\n" +
-                                "Tip: bruteforce search for upper bound > 300 is out of reach.",
+                                "Tip: bruteforce search for upper bound > 300 may take long.\n" +
+                                "Tip: bruteforce search for upper bound > 600 is out of reach.",
                                 "Invalid positive integer.\n")
 
     if lower_bound > upper_bound:
-        print("\nLower bound is higher than upper bound, so the bounds will be swapped.\n")
+        print("\nLower bound is higher than upper bound, so they will be swapped.\n")
         lower_bound, upper_bound = upper_bound, lower_bound
 
     add_all_labels(upper_bound)
@@ -260,12 +272,12 @@ def main():
         random_search(lower_bound, upper_bound,
                       get_int_input("\nChoose a positive integer - number of iterations:",
                                     "Invalid positive integer.\n"))
-    write_buffer()
+    show_graph(lower_bound)
 
     print("\n" + str(len(solutions) - len(correct_solutions)), "solutions with six squares.\n")
-    print(len(correct_solutions), "solutions with at least seven squares:")
+    print(len(correct_solutions), "solutions with at least seven squares.")
     for i in correct_solutions:
-        print(" ".join(map(str, i[0])))
+        print(" ".join(map(str, i[0][1])))
         print("| u1 =", i[1],
               "| v1 =", i[2],
               "| u2 =", i[3],
