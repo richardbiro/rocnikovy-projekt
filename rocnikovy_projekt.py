28ce3ac522e8d26ee20f199e57730b7d5dbf39cb
Richard Biro <biro.risko@gmail.com>
2020-06-17 00:35:52 +0200

    pridana dokumentacia, vylepsene random vyhladavanie, pouzitie argparseru

--- rocnikovy_projekt.py
+++ rocnikovy_projekt.py
@@ -1,3 +1,10 @@
+#Program spustite cez prikazovy riadok / terminal prikazom:
+#python rocnikovy_projekt.py [typ vyhladavania] [dolna hranica] [horna hranica]
+#Typ vyhladavania moze byt "bruteforce" (uplne) alebo "random" (nahodne)
+#Ak si zvolite nahodne vyhladavanie, mozete si zvolit aj pocet opakovani (defaultne je to 10^7):
+#python rocnikovy_projekt.py random [dolna hranica] [horna hranica] --iterations [pocet opakovani]
+
+from argparse import ArgumentParser
 from cProfile import runctx
 from datetime import datetime
 from math import gcd, isqrt
@@ -6,23 +13,26 @@ from numpy import arange
 from random import randint
 from multiprocessing import Pool
 
+#Meno suboru, do ktoreho zapisujeme magicke stvorce s 6 a viac stvorcami
 file = str(datetime.now())
 file = file[:file.index(".")]
 file = file.replace(":", "-")
 file = file + ".txt"
 
+#Meno suboru, do ktoreho zapisujeme magicke stvorce s 7 a viac stvorcami
 file_correct = str(datetime.now())
 file_correct = file_correct[:file_correct.index(".")]
 file_correct = file_correct.replace(":", "-")
 file_correct = file_correct + " correct.txt"
 
 solutions = dict()
-correct_solutions = []
+correct_solutions = dict()
 relatively_prime = []
 labels = []
 six_squares = []
 seven_squares = []
 
+#Funkcia vezme pole cisel a vrati ich najmensieho spolocneho delitela
 def gcd_array(array):
     if len(array) < 2:
         return None
@@ -30,6 +40,7 @@ def gcd_array(array):
     for i in range(2, len(array)): d = gcd(d, array[i])
     return d
 
+#Funkcia overi, ci je dane cislo stvorec
 def is_square(n):
     if n < 0:
         return False
@@ -38,12 +49,14 @@ def is_square(n):
 def get_pairs():
     return relatively_prime
 
+#Funkcia inicializuje vsetky polia, do ktorych sa budu ukladat data urcene pre vysledny graf
 def add_all_labels(upper_bound):
     for i in range(upper_bound+1):
         labels.append(i)
         six_squares.append(0)
         seven_squares.append(0)
-        
+
+#Funkcia hodi do pola vsetky dvojice nesudelitelnych cisel a utriedi podla maxima - vid cast 6
 def add_all_pairs(upper_bound):
     for i in range(1, upper_bound+1):
         for j in range(i, upper_bound+1):
@@ -56,6 +69,7 @@ def add_all_pairs(upper_bound):
 def get_solutions():
     return solutions
 
+#Funkcia vrati jednoznacny retazec (identifikator) magickeho stvorca - vid cast 6 a vetu 6.1
 def get_index(array):
     corners = [array[0], array[2], array[6], array[8]]
     corners.sort()
@@ -64,8 +78,8 @@ def get_index(array):
     center = array[4]
     return ' '.join(str(s) for s in [corner1, corner2, center])
 
+#Funkcia prida a zapise dany magicky stvorec (ak este nebol najdeny) - vid cast 6
 def add_solution(array, uv):
-    global buffer
     u1 = uv[0]
     v1 = uv[1]
     u2 = uv[2]
@@ -75,8 +89,7 @@ def add_solution(array, uv):
     index = get_index(square)
     largest = max(u1, v1, u2, v2)
 
-    if (index not in solutions or
-            list(map(int, solutions[index].split(' ')))[0] > largest):
+    if index not in solutions:
         with open(file, 'a') as f:
             f.write(str(str(squares) + " squares | u1 = " + str(u1) +
                         " | v1 = " + str(v1) + " | u2 = " + str(u2) +
@@ -87,17 +100,19 @@ def add_solution(array, uv):
             six_squares[largest] += 1
         else:
             seven_squares[largest] += 1
-            if [array, u1, v1, u2, v2] not in correct_solutions:
-                correct_solutions.append([array, u1, v1, u2, v2])
+            if index not in correct_solutions:
+                correct_solutions[index] = [array, u1, v1, u2, v2]
                 with open(file_correct, 'a') as f:
                     f.write(str(str(squares) + " squares | u1 = " + str(u1) +
                                 " | v1 = " + str(v1) + " | u2 = " + str(u2) +
                                 " | v2 = " + str(v2) + " | " + str(square) + "\n"))
 
+#Funkcia da stvorec do normalneho tvaru (vykrati cleny a umocni ich na druhu) - vid cast 6
 def normalize(array, divisor):
     for i in range(len(array)):
         array[i] = (array[i]//divisor)**2
 
+#Funkcia vygeneruje vsetky stvorce pre dane u1,v1,u2,v2 a zapamata si vyhovujuce - vid cast 6
 def run_configuration(uv):
     u1 = uv[0]
     v1 = uv[1]
@@ -152,27 +167,51 @@ def run_configuration(uv):
                 answers.append([counter, S])
     return [answers, [u1, v1, u2, v2]]
 
+#Funkcia zarovna text na zadanu dlzku
 def align(data, length):
     return (length - len(str(data)))*" " + str(data)
 
+#Funkcia prevedie cas zo sekund na format hodiny:minuty:sekundy
 def seconds_to_time(sec):
     answer = str(sec//3600) + "h "
     answer += str((sec%3600)//60) + "m "
     answer += str((sec%3600)%60) + "s"
     return answer
 
+#Funkcia prejde paralelne vsetky konfiguracie v execute-liste
+def execute_parallel(execute_list):
+    start = datetime.now()
+    p = Pool()
+    results = p.map(run_configuration, execute_list)
+    for i in range(len(results)):
+        for S in results[i][0]:
+            if S is not None:
+                add_solution(S, results[i][1])
+    p.close()
+    p.join()
+    end = datetime.now()
+    return end - start
+
+#Funkcia vypise priebeznu spravu (progress bar)
+def update(completed, block, allsteps, diff):
+    current = min(allsteps,completed*block)
+    print(align(int(100*current/allsteps), 3), "% |", align(current, len(str(allsteps))),
+          "out of", allsteps, "| estimated remaining time:",
+          align(seconds_to_time(int(diff.seconds*(allsteps - current)/block)), 12)) 
+
+#Funkcia prejde cez vsetky pary dvojic (u1,v1) a (u2,v2) - vid cast 6
 def run_all_pairs(lower_bound):
     n = len(relatively_prime)
     low = 0
     while max(relatively_prime[low]) < lower_bound:
         low += 1
+        
     allsteps = n*(n-1)//2 - low*(low-1)//2
+    completed = 0
     execute_list = []
     block = 10**6
-    blocks = allsteps//block + 1
-    completed = 0
+    
     print("\nSTART |", str(datetime.now()))
-
     for i1 in range(n):
         u1 = relatively_prime[i1][0]
         v1 = relatively_prime[i1][1]
@@ -181,25 +220,32 @@ def run_all_pairs(lower_bound):
             v2 = relatively_prime[i2][1]
             execute_list.append([u1, v1, u2, v2])
             if len(execute_list) > block or (i1 == n-2 and i2 == n-1):
-                start = datetime.now()
-                p = Pool()
-                results = p.map(run_configuration, execute_list)
-                for i in range(len(results)):
-                    for S in results[i][0]:
-                        if S is not None:
-                            add_solution(S, results[i][1])
-                p.close()
-                p.join()
+                diff = execute_parallel(execute_list)
                 execute_list = []
                 completed += 1
-                end = datetime.now()
-                diff = end - start
-                print(align(int(100*(completed*block)/allsteps), 3), "% |",
-                      align(completed*block, len(str(allsteps))), "out of", allsteps,
-                      "| estimated remaining time:", align(seconds_to_time(
-                          int(diff.seconds*(allsteps - completed*block)/block)), 12))    
+                update(completed, block, allsteps, diff)          
     print("END |", str(datetime.now()), "\n")
 
+#Funkcia prejde k-krat cez nahodne pary dvojic (u1,v1) a (u2,v2) - vid cast 6
+def run_random_pairs(lower_bound, upper_bound, k):
+    allsteps = k
+    completed = 0
+    execute_list = []
+    block = 10**6
+    
+    print("\nSTART |", str(datetime.now()))
+    for i in range(k):
+        first_pair = random_relatively_prime(lower_bound, upper_bound)
+        second_pair = random_relatively_prime(lower_bound, upper_bound)
+        execute_list.append(first_pair + second_pair)
+        if len(execute_list) > block or i == k-1:
+            diff = execute_parallel(execute_list)
+            execute_list = []
+            completed += 1
+            update(completed, block, allsteps, diff)      
+    print("END |", str(datetime.now()), "\n")
+
+#Funkcia ukaze vysledny graf pocetnosti magickych stvorcov
 def show_graph(lower_bound):
     x = arange(len(labels[lower_bound:]))
     width = 0.35
@@ -216,6 +262,7 @@ def show_graph(lower_bound):
     fig.tight_layout()
     plt.show()
 
+#Funkcia vygeneruje nahodnu usporiadanu dvojicu nesudelitelnych cisel
 def random_relatively_prime(lower_bound, upper_bound):
     first = randint(lower_bound, upper_bound)
     second = randint(lower_bound, upper_bound)
@@ -224,54 +271,32 @@ def random_relatively_prime(lower_bound, upper_bound):
     divisor = gcd(first, second)
     return [first//divisor, second//divisor]
 
+#Funkcia spusti bruteforce vyhladavanie od lower_bound po upper_bound
 def fixed_search(lower_bound, upper_bound):
     add_all_pairs(upper_bound)
     runctx('r(l)', {'r': run_all_pairs}, {'l': lower_bound})
-    
+
+#Funkcia spusti nahodne vyhladavanie od lower_bound po upper_bound s k opakovaniami
 def random_search(lower_bound, upper_bound, k):
-    for _ in range(k):
-        first_pair = random_relatively_prime(lower_bound, upper_bound)
-        second_pair = random_relatively_prime(lower_bound, upper_bound)
-        result = run_configuration(first_pair + second_pair)
-        for S in result[0]:
-            if S is not None:
-                add_solution(S, result[1])
-
-def get_input(text, answers, error):
-    while True:
-        print(text)
-        answer = input()
-        if answer in answers:
-            return answer
-        print(error)
-
-def get_int_input(text, error):
-    while True:
-        print(text)
-        try:
-            value = int(input())
-            return value
-        except ValueError:
-            print(error)
+    runctx('r(l, u, k)', {'r': run_random_pairs}, {'l': lower_bound, 'u': upper_bound, 'k': k})           
 
 def main():
-    search = get_input("Type 'bruteforce' to search all solutions.\n" +
-                       "Type 'random' to search random solutions.\n",
-                       ["bruteforce", "random"], "Invalid command.\n")
-
-    lower_bound = get_int_input("\nChoose a positive integer - lower bound for solutions:",
-                                "Invalid positive integer.\n")
-
-    upper_bound = get_int_input("\nChoose a positive integer - upper bound for solutions:\n" +
-                                "Tip: bruteforce search for upper bound > 300 may take long.\n" +
-                                "Tip: bruteforce search for upper bound > 600 is out of reach.",
-                                "Invalid positive integer.\n")
-
-    if lower_bound > upper_bound:
+    parser = ArgumentParser(description="search for a magic square within given bounds " +
+                            "(maximum of u1,v1,u2,v2)")
+    parser.add_argument("search", help="type of search (bruteforce or random)")
+    parser.add_argument("lower_bound", help="lower bound for solutions", type=int)
+    parser.add_argument("upper_bound", help="upper bound for solutions", type=int)
+    parser.add_argument("--iterations", default = 10**7, help="number of iterations in random " +
+                        "search (10^7 by default)", type=int)
+    args = parser.parse_args()
+    args.lower_bound = abs(args.lower_bound)
+    args.upper_bound = abs(args.upper_bound)
+    
+    if args.lower_bound > args.upper_bound:
         print("\nLower bound is higher than upper bound, so they will be swapped.\n")
-        lower_bound, upper_bound = upper_bound, lower_bound
+        args.lower_bound, args.upper_bound = args.upper_bound, args.lower_bound
 
-    add_all_labels(upper_bound)
+    add_all_labels(args.upper_bound)
 
     with open(file, 'w') as f:
         f.write("")
@@ -279,22 +304,24 @@ def main():
     with open(file_correct, 'w') as f:
         f.write("")
 
-    if search == "bruteforce":
-        fixed_search(lower_bound, upper_bound)
-    elif search == "random":
-        random_search(lower_bound, upper_bound,
-                      get_int_input("\nChoose a positive integer - number of iterations:",
-                                    "Invalid positive integer.\n"))
-    show_graph(lower_bound)
+    if args.search == "bruteforce":
+        fixed_search(args.lower_bound, args.upper_bound)
+    elif args.search == "random":
+        random_search(args.lower_bound, args.upper_bound, args.iterations)
+    else:
+        print("\nInvalid command: " + args.search + ".\n")
+        return
+    
+    show_graph(args.lower_bound)
 
     print("\n" + str(len(solutions) - len(correct_solutions)), "solutions with six squares.\n")
     print(len(correct_solutions), "solutions with at least seven squares.")
-    for i in correct_solutions:
-        print(" ".join(map(str, i[0][1])))
-        print("| u1 =", i[1],
-              "| v1 =", i[2],
-              "| u2 =", i[3],
-              "| v2 =", i[4], "|\n")
+    for i,j in correct_solutions.items():
+        print(" ".join(map(str, j[0][1])))
+        print("| u1 =", j[1],
+              "| v1 =", j[2],
+              "| u2 =", j[3],
+              "| v2 =", j[4], "|\n")
     input("\nPress any key to end.")
 
 if __name__ == '__main__':
--- rocnikovy_projekt_test.py
+++ rocnikovy_projekt_test.py
@@ -1,8 +1,7 @@
-from math import gcd, sqrt
+from math import gcd, isqrt
 from random import randint
-from rocnikovy_projekt import is_square, add_all_pairs, get_pairs, evaluate_square
+from rocnikovy_projekt import is_square, add_all_pairs, get_pairs
 from unittest import main, TestCase
-
 
 class SquareTest(TestCase):
     def setUp(self):
@@ -15,28 +14,23 @@ class SquareTest(TestCase):
             n = randint(1, self.square_range)
             self.assertTrue(is_square(n**2))
 
-
     def test_square_random_positive_not_square(self):
         for _ in range(self.iterations):
             m = randint(2, self.square_range)
             n = randint(m**2 + 1, (m + 1)**2 - 1)
             self.assertFalse(is_square(n))
 
-
     def test_square_random_negative(self):
         for _ in range(self.iterations):
             n = - randint(1, self.square_range)
             self.assertFalse(is_square(n))
 
-
     def test_square_zero(self):
         self.assertTrue(is_square(0))
-
 
     def test_relatively_prime_one_one_relatively_prime(self):
         add_all_pairs(1)
         self.assertTrue([1, 1] in get_pairs())
-
 
     def test_relatively_prime_random_relatively_prime(self):
         add_all_pairs(self.relatively_prime_range+1)
@@ -46,103 +40,19 @@ class SquareTest(TestCase):
             d = gcd(a, b)
             a = a//d
             b = b//d
+            if a > b:
+                a, b = b, a
             self.assertTrue([a, b] in get_pairs())
-
 
     def test_relatively_prime_random_not_relatively_prime(self):
         add_all_pairs(self.relatively_prime_range+1)
         for _ in range(self.iterations):
-            d = randint(2, int(sqrt(self.relatively_prime_range)))
+            d = randint(2, isqrt(self.relatively_prime_range))
             a = d*randint(1, self.relatively_prime_range//d)
             b = d*randint(1, self.relatively_prime_range//d)
+            if a > b:
+                a, b = b, a
             self.assertTrue([a, b] not in get_pairs())
-
-
-
-    def test_evaluate_simple_square(self):
-        solution = [2, 7, 6,
-                    9, 5, 1,
-                    4, 3, 8]
-        
-        self.assertTrue(evaluate_square([2, None, 6,
-                                         None, 5, None,
-                                         4, None, 8]) == solution)
-        
-        self.assertTrue(evaluate_square([2, 7, None,
-                                         None, 5, None,
-                                         None, 3, 8]) == solution)
-        
-        self.assertTrue(evaluate_square([None, 7, None,
-                                         9, 5, 1,
-                                         None, 3, None]) == solution)
-
-        
-
-    def test_evaluate_seven_squares_square(self):
-        solution = [42025, 277729, 222121,
-                    360721, 180625, 529,
-                    139129, 83521, 319225]
-        
-        self.assertTrue(evaluate_square([42025, None, 222121,
-                                         None, 180625, None,
-                                         139129, None, 319225]) == solution)
-        
-        self.assertTrue(evaluate_square([42025, 277729, None,
-                                         None, 180625, None,
-                                         None, 83521, 319225]) == solution)
-        
-        self.assertTrue(evaluate_square([None, 277729, None,
-                                         360721, 180625, 529,
-                                         None, 83521, None]) == solution)
-
-        
-
-    def test_evaluate_random_acegi_square(self):
-        for _ in range(self.iterations):
-            x = randint(1, self.square_range)
-            y = randint(1, self.square_range)
-            z = randint(1, self.square_range)
-
-            solution = [2*x+1, 6*z-2*x-2*y+1, 2*y+1,
-                        2*z-2*x+2*y+1, 2*z+1, 2*z+2*x-2*y+1,
-                        4*z-2*y+1, 2*x+2*y-2*z+1, 4*z-2*x+1]
-            
-            self.assertTrue(evaluate_square([2*x+1, None, 2*y+1,
-                                             None, 2*z+1, None,
-                                             4*z-2*y+1, None, 4*z-2*x+1]) == solution)
-
-
-    def test_evaluate_random_abehi_square(self):
-        for _ in range(self.iterations):
-            x = randint(1, self.square_range)
-            y = randint(1, self.square_range)
-            z = randint(1, self.square_range)
-
-            solution = [2*x+1, 2*y+1, 6*z-2*x-2*y+1,
-                        8*z-4*x-2*y+1, 2*z+1, 4*x+2*y-4*z+1,
-                        2*x+2*y-2*z+1, 4*z-2*y+1, 4*z-2*x+1]
-            
-            self.assertTrue(evaluate_square([2*x+1, 2*y+1, None,
-                                             None, 2*z+1, None,
-                                             None, 4*z-2*y+1, 4*z-2*x+1]) == solution)
-
-
-
-    def test_evaluate_random_bdefh_square(self):
-        for _ in range(self.iterations):
-            x = randint(1, self.square_range)
-            y = randint(1, self.square_range)
-            z = randint(1, self.square_range)
-
-            solution = [4*z-x-y+1, 2*x+1, 2*z-x+y+1,
-                        2*y+1, 2*z+1, 4*z-2*y+1,
-                        2*z+x-y+1, 4*z-2*x+1, x+y+1]
-            
-            self.assertTrue(evaluate_square([None, 2*x+1, None,
-                                             2*y+1, 2*z+1, 4*z-2*y+1,
-                                             None, 4*z-2*x+1, None]) == solution)
-    
-
 
 if __name__ == '__main__':
     main()
